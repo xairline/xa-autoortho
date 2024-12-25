9516519f88f78743400e86dab1b7970098c96c35
GH Release <release@opensam.com>
2024-12-25 20:32:30 +0000

    Release

---
+++ release/xa-autoortho/arm/perm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script recursively removes quarantine flags from all files in the current directory
+
+echo "Removing quarantine flags recursively in the current directory..."
+
+ROOT="$(pwd)"
+
+# find current script folder
+DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Find all files and directories with quarantine attribute and remove the attribute in the current directory
+cd $DIR
+find . -exec xattr -d com.apple.quarantine {} \; 2>/dev/null
+cd $ROOT
+echo "Quarantine flags removed."
